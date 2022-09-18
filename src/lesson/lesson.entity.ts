1c436862d5897bc157e638e4632dbea3b2d9d6d3
unknown <gowtham>
2022-09-19 00:13:31 +0530

    initial commit for repo

---
+++ src/lesson/lesson.entity.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, ObjectIdColumn, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Lesson {
+    @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  startDate: string;
+
+  @Column()
+  endDate: string;
+}
---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Lesson } from './lesson.entity';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateLessonInput } from './lesson.input';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOneBy({ id });
+  }
+
+  async getLessons(): Promise<Lesson[]> {
+    return this.lessonRepository.find();
+  }
+
+  async createLesson(createLesonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLesonInput;
+    const lesson = this.lessonRepository.create({
+      id: uuidv4(),
+      name,
+      startDate,
+      endDate,
+    });
+
+    this.lessonRepository.save(lesson);
+
+    return lesson;
+  }
+}
